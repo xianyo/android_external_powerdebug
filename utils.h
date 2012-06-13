da29aad876b1141bb78e733b2475b22688a3e59e
Lin Fuzhen <fuzhen.lin@freescale.com>
2012-06-13 19:09:53 +0800

    ENGR00174062-1 powerdebug: import powerdebug from Linaro
    import powerdebug0.6.1 from Linaro
    
    Signed-off-by: Lin Fuzhen <fuzhen.lin@freescale.com>

---
+++ utils.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Linaro Limited.
+ *
+ * This file is part of PowerDebug.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Daniel Lezcano <daniel.lezcano@linaro.org> (IBM Corporation)
+ *       - initial API and implementation
+ *******************************************************************************/
+#ifndef __UTILS_H
+#define __UTILS_H
+
+extern int file_read_value(const char *path, const char *name,
+                           const char *format, void *value);
+
+
+#endif
