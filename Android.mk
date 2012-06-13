8345008feab1776e4e5a1d4c3f57b60038b263a7
Lin Fuzhen <fuzhen.lin@freescale.com>
2012-06-13 19:09:53 +0800

    ENGR00174062-1 powerdebug: import powerdebug from Linaro
    import powerdebug0.6.1 from Linaro
    
    Signed-off-by: Lin Fuzhen <fuzhen.lin@freescale.com>

---
+++ Android.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := libstlport
+
+LOCAL_STATIC_LIBRARIES := libncurses
+
+LOCAL_MODULE := powerdebug
+
+LOCAL_CPPFLAGS += \
+		-DDISABLE_I18N \
+		-DDISABLE_TRYCATCH \
+		-DNCURSES_NOMACROS \
+		-DDISABLE_WSTRING \
+		-DDEFAULT_TERM=\"xterm\" \
+		-DTERMINFO_PATH=\"/system/etc/terminfo\" \
+		-DDEFINE_ETHTOOL_CMD \
+
+LOCAL_C_INCLUDES += external/stlport/stlport/ \
+					external/stlport/stlport/stl \
+					external/stlport/stlport/using/h/ \
+					bionic \
+					external/ncurses \
+					external/ncurses/lib \
+					external/ncurses/include \
+					external/ncurses/include/ncurses
+
+LOCAL_SRC_FILES += \
+	powerdebug.c sensor.c clocks.c regulator.c \
+	display.c tree.c utils.c mainloop.c
+
+include $(BUILD_EXECUTABLE)
